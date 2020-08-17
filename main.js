d31dd437d3864fd5f4ccf0bf3a20bf0347a10c24
mamatovp <58777435+mamatovp@users.noreply.github.com>
2020-08-17 20:34:10 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+alert('Привет Марафонец');
+console.log('Привет Марафонец');
+
+let firstName = 'Pavel';
+
+console.log(firstName);
+
+firstName = 'Pasha';
+console.log(firstName);
+
+const lastName = 'Mamatov';
+
+console.log(lastName);
+
+console.log(typeof firstName);
+
+
