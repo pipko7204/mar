59ad6e35514e8de84229ff22d6eeaf8912a3697b
Pavel Mamatov <pmamatov@icloud.com>
2020-08-21 00:15:18 +0300

    2 stage of the marathon

--- main.js
+++ main.js
@@ -7,20 +7,17 @@ const a = 'а';
 let firstRowA = 0;
 let secondRowA = 0;
 
-for (i = 0; i < firstRow.length;i++) {
+for (let i = 0; i < firstRow.length;i++) {
     if (firstRow[i] == a ){
         firstRowA++
     }
 }
 
-for (i = 0; i < secondRow.length;i++) {
+for (let i = 0; i < secondRow.length;i++) {
     if (secondRow[i] == a ){
         secondRowA++
     }
 }
-
-console.log(firstRowA);
-console.log(secondRowA);
 
 if (firstRowA > secondRowA) {
     console.log(firstRow);
@@ -32,26 +29,22 @@ if (firstRowA > secondRowA) {
 
 const phoneNumber = '+71234567890'; // =>  '+7 (123) 456-78-90'
 
-let changedNumber = phoneNumber;
+function numberOfDigits(number) {
+    let wewe = number.length;
+    return wewe;
+}
 
-changedNumber[12] = changedNumber[18];
-changedNumber[11] = changedNumber[17];
-changedNumber[10] = changedNumber[15];
-changedNumber[9] = changedNumber[14];
-changedNumber[8] = changedNumber[12];
-changedNumber[7] = changedNumber[11];
-changedNumber[6] = changedNumber[10];
-changedNumber[5] = changedNumber[7];
-changedNumber[4] = changedNumber[6];
-changedNumber[3] = changedNumber[5];
-
-changedNumber[3] = ' '
-changedNumber[4] =
-changedNumber[8] =
-changedNumber[9] =
-changedNumber[13] =
-changedNumber[16] =
-
+let changedNumber = '';
+for(let i = 0; i < phoneNumber.length; i++) {
+    changedNumber += phoneNumber.charAt(i);
+    if (i == 1) {
+        changedNumber += ' ('
+    } else if (i == 4) {
+        changedNumber += ') '
+          } else if (i == 7) {
+        changedNumber += '-';
+    } else if (i == 9) {
+        changedNumber += '-';
+    }
+}
 console.log(changedNumber);
-
-
